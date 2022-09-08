2d80da9d584fb9ff3e400977884b3ded52f1082a
Adson Gomes Oliveira <agdev@DESKTOP-0N655A3.localdomain>
2022-09-08 06:53:00 -0300

    adding typescript option

--- bin/index.js
+++ bin/index.js
@@ -3,15 +3,13 @@ const readlineSync = require('readline-sync');
 const createProjectPaths = require('../src/createProjectPaths');
 const setupDependencies = require('../src/setupDependencies');
 
-const languages = ['Typescript', 'Javascript'];
-
 const projectName = process.argv.slice(2);
-const language = readlineSync.keyInSelect(languages, 'Which will be the app language?');
+const language = readlineSync.keyInYN('Do you will use Typescript?');
 const orm = readlineSync.keyInYN('Do you will use the ORM Sequelize?');
 
 async function execute() {
-  await createProjectPaths(projectName, orm);
-  await setupDependencies(projectName, orm);
+  await createProjectPaths(projectName, orm, language);
+  await setupDependencies(projectName, orm, language);
 }
 
 execute();
--- src/createProjectPaths.js
+++ src/createProjectPaths.js
@@ -1,21 +1,6 @@
 const fs = require('@supercharge/filesystem');
-async function createProjectPaths(projectName, condSequelize) {
+async function createProjectPaths(projectName, condSequelize, typescript) {
   const paths = [];
-
-  if (condSequelize) {
-    paths.push( 
-    `./${projectName}/src`,
-    `./${projectName}/src/routes`,
-    `./${projectName}/src/routes`,
-    `./${projectName}/src/validations`,
-    `./${projectName}/src/services`,
-    `./${projectName}/src/controllers`,
-    `./${projectName}/src/helpers`,
-    `./${projectName}/src/middlewares`,
-    `./${projectName}/src/tests`,
-    `./${projectName}/src/database`,
-    );
-  }
 
   paths.push( 
     `./${projectName}/src`,
@@ -28,7 +13,10 @@ async function createProjectPaths(projectName, condSequelize) {
     `./${projectName}/src/helpers`,
     `./${projectName}/src/middlewares`,
     `./${projectName}/src/tests`,
-  );
+    );
+    
+  if (condSequelize) paths.push(`./${projectName}/src/database`);
+  if (typescript) paths.push(`./${projectName}/src/interfaces`, `./${projectName}/src/types`);
 
   const promises = [];
   paths.forEach((path) => {promises.push(fs.ensureDir(path))});
